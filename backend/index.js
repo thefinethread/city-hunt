b14dc22706d25ce1f218678905aec5b1b4dfac89
Kartikey Shukla <kartikey8604@gmail.com>
2024-04-18 23:51:06 +0530

    added dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+require('dotenv').config();
 const path = require('path');
 const appRouter = require('./routes/app.route');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
